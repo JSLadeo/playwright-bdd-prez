cb134fc00a5470daa2402f7e6fe9ee36d2384346
Julien S <simphal.julien@gmail.com>
2025-07-10 14:56:45 +0200

    feat(): api to finish

--- examples/api-testing/features/steps/fixtures.ts
+++ examples/api-testing/features/steps/fixtures.ts
@@ -1,14 +1,19 @@
-import { APIResponse } from '@playwright/test';
+import { APIResponse, Clock } from '@playwright/test';
 import { test as base, createBdd } from 'playwright-bdd';
 
 type Fixtures = {
   ctx: { response: APIResponse };
+  clockWait: { wait: Clock };
 };
 
 export const test = base.extend<Fixtures>({
   ctx: async ({}, use) => {
     const ctx = {} as Fixtures['ctx'];
     await use(ctx);
+  },
+  clockWait: async ({}, use) => {
+    const clockWait = {} as Fixtures['clockWait'];
+    await use(clockWait);
   },
 });
 
--- examples/api-testing/features/steps/index.ts
+++ examples/api-testing/features/steps/index.ts
@@ -1,16 +1,38 @@
 import { expect } from '@playwright/test';
-import { When, Then } from './fixtures';
+import { When, Then, Given } from './fixtures';
 
 When('GET {string}', async ({ request, ctx }, url: string) => {
   ctx.response = await request.get(url);
+  const requestUrl = await request.get(url);
+  console.log('header age:', requestUrl.headers().age);
+  // console.log('body:', await requestUrl.json());
+  console.log('body:', await ctx.response.body());
+  // console.log('body:', await requestUrl.text());
+
+  //example classic fetch get
+  // await request.get('https://example.com/api/getText', {
+  //   params: {
+  //     'isbn': '1234',
+  //     'page': 23,
+  //   }
+  // });
 });
 
 When('POST {string}', async ({ request, ctx }, url: string, data: string) => {
   ctx.response = await request.post(url, { data });
+  //Example classic fetch post
+  // await request.fetch('https://example.com/api/createBook', {
+  //   method: 'post',
+  //   data: {
+  //     title: 'Book Title',
+  //     author: 'John Doe',
+  //   }
+  // });
 });
 
 Then('status is {int}', async ({ ctx }, status: number) => {
   expect(ctx.response.status()).toEqual(status);
+  expect(ctx.response.status()).toEqual(301);
 });
 
 Then('response has prop {string}', async ({ ctx }, keyPath: string) => {
